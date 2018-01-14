e658f29a1e7b8652f45a12a8caf0ddacd11e1e0f
Caroline Lee <carolinel2610@gmail.com>
2018-01-14 19:18:09 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each {|a,b|
-    if a==:suite_a && b.start_with"A"
+    if a==:suite_a && b.start_with?("A")
       winner = b
   }
   winner
